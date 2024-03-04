e5e99321d9517bb3555409dc9f3e12c4c8c52092
Tobias Binna <tobias.binna@toolsplus.io>
2024-03-04 12:39:50 +0800

    Initial commit

---
+++ apps/nx-forge-app-webpack-e2e/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'nx-forge-app-webpack-e2e',
+  preset: '../..//jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../..//coverage/nx-forge-app-webpack-e2e',
+};
