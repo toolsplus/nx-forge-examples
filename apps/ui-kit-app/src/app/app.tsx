0b2fccd52823cbaa3fd22dfbffff6c3615eb2abe
Tobias Binna <tobias.binna@toolsplus.io>
2024-07-03 15:37:49 +0800

    feat: add UI Kit example

---
+++ apps/ui-kit-app/src/app/app.tsx
@@ -0,0 +1,4 @@
+import { Text } from '@forge/react';
+
+const App = () => <Text>👋 Hello world!</Text>;
+export default App;
---
+++ apps/ui-kit-app/webpack.config.js
@@ -0,0 +1,31 @@
+const { NxWebpackPlugin } = require('@nx/webpack');
+const { NxReactWebpackPlugin } = require('@nx/react');
+const { join } = require('path');
+
+module.exports = {
+  output: {
+    path: join(__dirname, '../../dist/apps/ui-kit-2-app-3'),
+  },
+  devServer: {
+    port: 4200,
+  },
+  plugins: [
+    new NxWebpackPlugin({
+      tsConfig: './tsconfig.app.json',
+      compiler: 'babel',
+      main: './src/main.tsx',
+      baseHref: '/',
+      assets: ['./src/assets'],
+      styles: [],
+      commonChunk: false,
+      runtimeChunk: false,
+      outputHashing: 'none',
+      optimization: process.env['NODE_ENV'] === 'production',
+    }),
+    new NxReactWebpackPlugin({
+      // Uncomment this line if you don't want to use SVGR
+      // See: https://react-svgr.com/
+      // svgr: false
+    }),
+  ],
+};
