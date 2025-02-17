2ebde522e34f1d3306190c147001b69e42899e33
Tobias Binna <tobias.binna@toolsplus.io>
2025-02-17 17:08:26 +0800

    chore: update to nx-forge 6.0.2
    add modal dialog resource

---
+++ apps/modal-dialog/src/app/app.tsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <h1>A modal dialog</h1>
+      <p>
+        Dolor minim voluptate dolore proident voluptate ea nostrud laboris ex
+        enim laboris. Do exercitation fugiat pariatur consequat consectetur. Ut
+        nulla nostrud ex dolore nostrud est voluptate sunt amet consectetur
+        cupidatat cillum anim exercitation quis. Velit ipsum ullamco incididunt
+        incididunt culpa adipisicing. Non proident reprehenderit et laborum
+        proident in aliqua do proident dolor ut ex nisi. Veniam cillum ipsum
+        cupidatat irure exercitation ex do voluptate commodo veniam minim minim
+        reprehenderit.
+      </p>
+    </StyledApp>
+  );
+}
+
+export default App;
